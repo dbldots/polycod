ee2cbbe769c53a61f4c1fc83936f08991e442d76
johannes-kostas goetzinger <dbldots@gmail.com>
2016-01-09 22:21:13 +0100

    updates demo

--- demo/app.js
+++ demo/app.js
@@ -7,14 +7,19 @@ angular.module('demo').run(function($rootScope) {
     $rootScope.gotActivated = true;
   }
   $rootScope.gotActivated = false;
+  $rootScope.weekDays = ['monday', 'tuesday', 'wednesday']
+  $rootScope.onClick = function() {
+    alert('clicked a day. is it your favourite day? i won\'t tell anyone.');
+  }
 });
 
 Polycod.component({
-  selector: 'foo',
+  selector: 'cmp1',
   module: 'demo',
   inject: ['$element'],
-  template: '<h1>hello {{ name }}</h1><small>(using name from binding)</small>',
+  template: '<h1>hello {{ name }}&nbsp;<small>(using name from binding)</small></h1><div transclude-id="info"></div>',
   events: ['activated', 'deactivated'],
+  transclude: true,
   class: function($element) {
     console.log('injected $element', $element);
 
@@ -22,4 +27,11 @@ Polycod.component({
       this.activated('yay')
     }.bind(this);
   }
+});
+
+Polycod.component({
+  selector: 'cmp2',
+  module: 'demo',
+  template: '<div><span *ng-for="#day of days" (click)="onClick()">{{ day }}&nbsp;</span></div>',
+  class: function() {}
 });

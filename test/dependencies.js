7402613f0f68499ac832685959a7251c394af1c3
johannes-kostas goetzinger <dbldots@gmail.com>
2016-01-12 23:06:29 +0100

    fixes phantomjs test runs by adding a .bind polyfill

--- test/dependencies.js
+++ test/dependencies.js
@@ -1,3 +1,11 @@
+// PhantomJS doesn't support bind yet
+Function.prototype.bind = Function.prototype.bind || function (thisp) {
+  var fn = this;
+  return function () {
+    return fn.apply(thisp, arguments);
+  };
+};
+
 window.jQuery = require('jquery');
 require('angular');
 require('angular-mocks');
